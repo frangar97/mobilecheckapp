c8700dbb3960c3f71f5b61f4761b342d636722d9
Francisco <frangarcia38@hotmail.com>
2022-09-22 18:17:23 -0600

    Commit con los siguientes cambios: - Se remueve dependencia de open maps. - Se coloca titulo en pantalla de listado de visitas.

--- src/screens/VisitaListScreen.tsx
+++ src/screens/VisitaListScreen.tsx
@@ -1,15 +1,30 @@
-import { useEffect, useState } from 'react';
-import { FlatList, useWindowDimensions } from 'react-native'
+import { FC, useEffect, useState } from 'react';
+import { FlatList, useWindowDimensions, Text, TouchableOpacity } from 'react-native'
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import axios from 'axios';
 import { apiURL } from '../constants';
 import { useUsuario } from '../store/useUsuario'
 import { Visita } from '../types/visita_types';
 import { VisitaCard } from '../components';
+import { AppNavigationType } from '../types/navigation_types';
 
-export const VisitaListScreen = () => {
+type props = NativeStackScreenProps<AppNavigationType, "cliente_list">
+
+export const VisitaListScreen: FC<props> = ({ navigation }) => {
     const token = useUsuario(e => e.token);
     const { height, width } = useWindowDimensions();
     const [visitas, setVisitas] = useState<Visita[]>([]);
+
+    useEffect(() => {
+        navigation.setOptions({
+            headerTitle: "Visitas",
+            headerShown: true,
+            headerRight: () => (
+                <TouchableOpacity onPress={() => navigation.goBack()} style={{ width: 30, height: 30, borderColor: "#cccc", borderWidth: 0.5, borderRadius: 4, justifyContent: "center", alignItems: "center" }}>
+                    <Text style={{ fontSize: 20 }}>+</Text>
+                </TouchableOpacity>),
+        })
+    }, [navigation]);
 
     const obtenerVisitasUsuario = async () => {
         try {
