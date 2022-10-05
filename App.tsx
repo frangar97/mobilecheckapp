deef8506e024e890ee31c5133eb1c09fe45c5a51
Francisco A. Garcia <frangarcia38@hotmail.com>
2022-10-05 15:56:29 -0600

    Obtencion de visitas del dia al abrir aplicacin

--- App.tsx
+++ App.tsx
@@ -2,11 +2,9 @@ import { useEffect } from 'react';
 import { SafeAreaView, LogBox } from 'react-native';
 import { NavigationContainer } from "@react-navigation/native";
 import { AppNavigation } from './src/navigation/AppNavigation';
-import { useUsuario } from './src/store/useUsuario';
 import { AuthNavigation } from './src/navigation/AuthNavigation';
 import { enableLatestRenderer } from 'react-native-maps';
-import { useCliente } from './src/store/useCliente';
-import { useTipoVisita } from './src/store/useTipoVisita';
+import { useTipoVisita, useCliente, useVisita, useUsuario } from './src/store';
 import SplashScreen from 'react-native-splash-screen';
 LogBox.ignoreAllLogs();
 enableLatestRenderer();
@@ -15,11 +13,13 @@ const App = () => {
   const { sesionActiva, _hasHydrated, token } = useUsuario();
   const obtenerClientes = useCliente(e => e.obtenerClientes);
   const obtenerTiposVisita = useTipoVisita(e => e.obtenerTiposVisita);
+  const obtenerVisitas = useVisita(e => e.obtenerVisitas);
 
   useEffect(() => {
     if (_hasHydrated && sesionActiva) {
       obtenerTiposVisita(token);
       obtenerClientes(token);
+      obtenerVisitas(token);
       SplashScreen.hide();
     }
 
