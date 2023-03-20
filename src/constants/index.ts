74fc485ced1262331a92c00cbf5e3e06c6b1fc71
Francisco A. Garcia <frangarcia38@hotmail.com>
2023-03-20 14:13:10 -0600

    Configuracion Intermoda

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,8 +1,8 @@
 import icons from "./icons";
 import colors from "./colors";
 import images from "./images";
-
-const apiURL = "http://localhost:8080";
+//passwordkey = Intermoda2020
+const apiURL = "http://18.219.36.66:8080";
 
 export {
     icons,
